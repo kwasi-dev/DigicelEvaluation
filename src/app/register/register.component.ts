d2320db2f229fe7ff46042aed8acd3a9868f9cbd
logan20 <kwasiedwards@gmail.com>
2019-06-04 18:29:28 -0400

    Finalised login and register background pic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -10,21 +10,14 @@ export class RegisterComponent implements OnInit {
   password: string;
   confirmPassword: string;
   email: string;
-  confirmEmail: string;
-
   constructor() { }
 
   ngOnInit() {
   }
 
   register(): void {
-    if (this.confirmEmail === undefined || this.email === undefined || this.confirmPassword === undefined
-      || this.password === undefined || this.username === undefined) {
+    if (this.email === undefined || this.confirmPassword === undefined || this.password === undefined || this.username === undefined) {
       alert('Please enter all fields!');
-      return;
-    }
-    if (this.email !== this.confirmEmail ) {
-      alert('Emails do not match!');
       return;
     }
     if (this.password !== this.confirmPassword){
