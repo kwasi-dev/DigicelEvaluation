fade88283a5d77088b6e2607748a71f39e9eee72
logan20 <kwasiedwards@gmail.com>
2019-06-05 10:18:10 -0400

    added login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,17 +8,40 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private http: HttpClient) { }
   username: string;
   password: string;
   ngOnInit() {
   }
   login(): void {
-    if (this.username === 'logan20') {
-      this.router.navigate(['home']);
-    } else {
-      alert ('TODO: login functionality: Username=' + this.username + ' Password= ' + this.password);
+    if (this.password === undefined || this.username === undefined) {
+      alert('Please enter all fields!');
+      return;
     }
+
+    this.http.post('http://127.0.0.1:5000/login', {
+        username: this.username,
+        password: this.password
+      },
+      {headers: { 'Content-Type': 'application/json' }}
+    ).subscribe(
+      res => {
+        const result = JSON.parse(JSON.stringify(res));
+        console.log(result);
+        if (result.status) {
+          // TODO: store login token
+          this.router.navigate(['home']);
+          console.log(result);
+        } else {
+          alert (result.message);
+        }
+        console.log(res);
+      },
+      it => {
+        console.log(it);
+        alert('An error occurred, please try again later');
+      }
+    );
   }
 
 }
